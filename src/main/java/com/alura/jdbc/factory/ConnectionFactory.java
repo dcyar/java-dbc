ead8f5015663f1dd2df0f6b704ada346ff72f54e
Jhon Achata <jhonachata.dev@gmail.com>
2023-04-20 21:56:36 -0500

    Update: update jdbc connection with pool connection

--- src/main/java/com/alura/jdbc/factory/ConnectionFactory.java
+++ src/main/java/com/alura/jdbc/factory/ConnectionFactory.java
@@ -1,15 +1,24 @@
 package com.alura.jdbc.factory;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+import javax.sql.DataSource;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionFactory {
+    private DataSource datasource;
+
+    public ConnectionFactory() {
+        var pooledDataSource = new ComboPooledDataSource();
+        pooledDataSource.setJdbcUrl("jdbc:mysql://localhost/control_de_stock?useTimeZone=true&serverTimeZone=UTC");
+        pooledDataSource.setUser("root");
+        pooledDataSource.setPassword("");
+
+        this.datasource = pooledDataSource;
+    }
+
     public Connection recuperaConexion() throws SQLException {
-        return DriverManager.getConnection(
-                "jdbc:mysql://localhost/control_de_stock?useTimeZone=true&serverTimeZone=UTC",
-                "root",
-                ""
-        );
+        return this.datasource.getConnection();
     }
 }
